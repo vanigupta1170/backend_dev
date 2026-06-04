7b9f34f65e19da7d8bd1707153bf59f6cd46f48d
Shibani Gupta <shibanigupta1170@gmail.com>
2026-06-05 00:40:31 +0530

    Modified the basic instagram page with static files and include sections and tried different properties related to it.

--- backend basics day 3_EJS/index.js
+++ backend basics day 3_EJS/index.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 const ports = 8080;
 
+
+app.use(express.static(path.join(__dirname, "/public")));
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "/views"));
 
@@ -16,8 +18,11 @@ app.get("/ig/:username", (req, res) => {
     let {username}=req.params;
     const instaData=require("./data.json");
     const data=instaData[username];
-    console.log(data);
+    if(data){
     res.render("instagram.ejs",{data});
+    }else{
+        res.render("error.ejs");
+    }
 });
 
 
