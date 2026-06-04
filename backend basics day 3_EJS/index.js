952fb65bf94b6b1f170c6d77ed74829029cc9f8f
Shibani Gupta <shibanigupta1170@gmail.com>
2026-06-04 23:55:39 +0530

    Created a rolldice system and learned about instagram EJS and also worked wit conditional statements in EJS

--- backend basics day 3_EJS/index.js
+++ backend basics day 3_EJS/index.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 
 const ports = 8080;
 
 app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "/views"));
+
+
 app.get('/', (req, res) => {
     res.render("home.ejs");
+});
+app.get("/ig/:username", (req, res) => {
+    let {username}=req.params;
+    const instaData=require("./data.json");
+    const data=instaData[username];
+    console.log(data);
+    res.render("instagram.ejs",{data});
+});
+
+
+
+app.get("/hello", (req, res) => {
+    res.send("Hello World");
+});
+app.get("/rolldice", (req, res) => {
+    let diceVal= Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice.ejs", {diceVal});
 });
 
 app.listen(ports, () => {
